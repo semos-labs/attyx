745e67decaf83079b73cc538e331055868510528
Nick Skriabin <nr@fenelon.ru>
2026-02-23 19:06:41 +0400

    Fix linux tests

--- build.zig
+++ build.zig
@@ -185,22 +185,20 @@ pub fn build(b: *std.Build) void {
     // A run step that will run the test executable.
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    // Creates an executable that will run `test` blocks from the executable's
-    // root module. Note that test executables only test one module at a time,
-    // hence why we have to create two separate ones.
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    // A run step that will run the second test executable.
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
-    // make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+
+    // exe_tests links the platform layer (Metal on macOS, GLFW/GL/FreeType on
+    // Linux) which requires GUI libraries. On macOS those frameworks are always
+    // present; on Linux they may be missing (headless CI). Skip exe_tests on
+    // Linux — all terminal engine tests are in mod_tests above.
+    if (target.result.os.tag != .linux) {
+        const exe_tests = b.addTest(.{
+            .root_module = exe.root_module,
+        });
+        const run_exe_tests = b.addRunArtifact(exe_tests);
+        test_step.dependOn(&run_exe_tests.step);
+    }
 
     // Just like flags, top level steps are also listed in the `--help` menu.
     //
