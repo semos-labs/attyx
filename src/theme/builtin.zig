21c0b2a98167a2be52d9d0aac3cbf6a08c3d4a8e
Nick Skriabin <nr@fenelon.ru>
2026-03-07 23:00:04 +0400

    Themes

--- src/theme/builtin.zig
+++ src/theme/builtin.zig
@@ -11,6 +11,26 @@ pub const BuiltinTheme = struct {
 };
 
 pub const builtins: []const BuiltinTheme = &.{
-    .{ .name = "default",          .content = themes_data.default          },
-    .{ .name = "catppuccin-mocha", .content = themes_data.catppuccin_mocha },
+    .{ .name = "default",            .content = themes_data.default            },
+    .{ .name = "catppuccin-latte",   .content = themes_data.catppuccin_latte   },
+    .{ .name = "catppuccin-mocha",   .content = themes_data.catppuccin_mocha   },
+    .{ .name = "dracula",            .content = themes_data.dracula            },
+    .{ .name = "everforest-dark",    .content = themes_data.everforest_dark    },
+    .{ .name = "github-dark",        .content = themes_data.github_dark        },
+    .{ .name = "gruvbox-dark",       .content = themes_data.gruvbox_dark       },
+    .{ .name = "gruvbox-light",      .content = themes_data.gruvbox_light      },
+    .{ .name = "iceberg",            .content = themes_data.iceberg            },
+    .{ .name = "kanagawa",           .content = themes_data.kanagawa           },
+    .{ .name = "material",           .content = themes_data.material           },
+    .{ .name = "monokai",            .content = themes_data.monokai            },
+    .{ .name = "nord",               .content = themes_data.nord               },
+    .{ .name = "one-dark",           .content = themes_data.one_dark           },
+    .{ .name = "palenight",          .content = themes_data.palenight          },
+    .{ .name = "rose-pine",          .content = themes_data.rose_pine          },
+    .{ .name = "rose-pine-moon",     .content = themes_data.rose_pine_moon     },
+    .{ .name = "snazzy",             .content = themes_data.snazzy             },
+    .{ .name = "solarized-dark",     .content = themes_data.solarized_dark     },
+    .{ .name = "solarized-light",    .content = themes_data.solarized_light    },
+    .{ .name = "tokyo-night",        .content = themes_data.tokyo_night        },
+    .{ .name = "tokyo-night-storm",  .content = themes_data.tokyo_night_storm  },
 };
--- themes/themes.zig
+++ themes/themes.zig
@@ -3,3 +3,23 @@
 
 pub const default: []const u8 = @embedFile("default.toml");
 pub const catppuccin_mocha: []const u8 = @embedFile("catppuccin-mocha.toml");
+pub const catppuccin_latte: []const u8 = @embedFile("catppuccin-latte.toml");
+pub const dracula: []const u8 = @embedFile("dracula.toml");
+pub const everforest_dark: []const u8 = @embedFile("everforest-dark.toml");
+pub const github_dark: []const u8 = @embedFile("github-dark.toml");
+pub const gruvbox_dark: []const u8 = @embedFile("gruvbox-dark.toml");
+pub const gruvbox_light: []const u8 = @embedFile("gruvbox-light.toml");
+pub const iceberg: []const u8 = @embedFile("iceberg.toml");
+pub const kanagawa: []const u8 = @embedFile("kanagawa.toml");
+pub const material: []const u8 = @embedFile("material.toml");
+pub const monokai: []const u8 = @embedFile("monokai.toml");
+pub const nord: []const u8 = @embedFile("nord.toml");
+pub const one_dark: []const u8 = @embedFile("one-dark.toml");
+pub const palenight: []const u8 = @embedFile("palenight.toml");
+pub const rose_pine: []const u8 = @embedFile("rose-pine.toml");
+pub const rose_pine_moon: []const u8 = @embedFile("rose-pine-moon.toml");
+pub const snazzy: []const u8 = @embedFile("snazzy.toml");
+pub const solarized_dark: []const u8 = @embedFile("solarized-dark.toml");
+pub const solarized_light: []const u8 = @embedFile("solarized-light.toml");
+pub const tokyo_night: []const u8 = @embedFile("tokyo-night.toml");
+pub const tokyo_night_storm: []const u8 = @embedFile("tokyo-night-storm.toml");
